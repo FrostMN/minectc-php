73a54219f6d180a1db4f098a14f54dd24d8e1875
Aaron <asouer@gmail.com>
2017-06-01 07:18:18 -0500

    Create css.php

---
+++ html/blocks/css.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo "<link href=\"css\style.css\" rel=\"stylesheet\" type=\"text/css\">";
+echo "<script src=\"https://use.fontawesome.com/6dd5a091da.js\"></script>";
+
