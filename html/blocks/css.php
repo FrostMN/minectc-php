5a74fe8995b211348eeb8e978226e25d0e5c9a86
Aaron <asouer@gmail.com>
2017-06-07 14:51:11 -0500

    Update css.php

--- html/blocks/css.php
+++ html/blocks/css.php
@@ -6,6 +6,6 @@
  * and open the template in the editor.
  */
 
+echo "<link href=\"css\reset.css\" rel=\"stylesheet\" type=\"text/css\">";
 echo "<link href=\"css\style.css\" rel=\"stylesheet\" type=\"text/css\">";
 echo "<script src=\"https://use.fontawesome.com/6dd5a091da.js\"></script>";
-
