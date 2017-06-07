738257993bdad32118f74700809f7d426a28af8b
Aaron <asouer@gmail.com>
2017-06-07 14:47:22 -0500

    Create home-div.php

---
+++ html/blocks/main/home-div.php
@@ -0,0 +1,34 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$home = "
+<div class=\"home-div\">
+    <div class=\"content-header\">
+        <img src=\"/img/grass.png\" alt=\"Grass Block\" style=\"width:80px;height:80px;float:left;\">
+        <h1>MineCTC: A Minecraft Server</h1>
+    </div>
+    <div class=\"content-body\">
+        <h3>Welcome to MineCTC</h3>
+        <p>
+        Thank you for checking out our lang page for the MineCTC server.
+        This page is still under active develompnt. If anyone has any expeirance 
+        with CSS help with styling the page would be appreciated. The following 
+        items are planned to be implemented:
+        </p>
+        <ul>
+            <li>Mass email to users for server notifications</li>
+            <li>Ticketing system for problems with the server</li>
+            <li>Forums</li>
+            <li>Cleaner/more cohesive CSS</li>
+        </ul>
+    </div>
+</div>
+";
+
+echo $home;
