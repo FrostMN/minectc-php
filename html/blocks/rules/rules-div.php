39033dd045d43c04a6a73b96282dcb3f80d89849
Aaron <asouer@gmail.com>
2017-06-07 14:47:55 -0500

    Create rules-div.php

---
+++ html/blocks/rules/rules-div.php
@@ -0,0 +1,105 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$rules = "
+<div class=\"rules-div\">
+    <h1>Code of Conduct for the MineCTC Server</h1>
+    <h3>(Which is sadly necessary it seems)</h3>
+    <ol>
+        <li>Server Admins are not obligated to tolerate harassment</li>
+            <ul>
+                <li>Playing here is a privilege and not a right.
+                <li>Server staff will only continue to keep the server running so long as there is motivation to do so, the server will not continue if maintaining it becomes a challenge to other commitments staff must attend to.
+                <li>Staff are not obligated to tolerate, hostilities, or harassment.
+                <li>If there is an objection to be raised, one is welcome to bring said objection to staff attention. Once. After the staff has decided on the objection it will be considered closed. Objections should be made using logical arguments only.
+                <li>Do not use expletives.
+                <li>Do not issue demands.
+                <li>Do not speak in an undue authority.
+                <li>Staff have other obligations on their time as well, as such it is expected that those who wish to play here respect that.
+                <li>If one does not wish to abide by these policies, one is more than welcome to make server elsewhere.
+            </ul>
+        <li>Staff will not take any action that benefits one player over another
+            <ul>
+                <li>Staff will not give players op status
+                <li>Staff will not release world files
+                <li>Staff will not release seed#
+                <li>Items lost due to staff culpability will be replaced, but players must bring such issues to staff attention within a reasonable timeframe.
+                <li>Disciplinary action is not included in this section, and will be executed as outlined in other sections.
+            </ul>
+        <li>When someone starts a build, it is their build. Do not modify it without permission.
+            <ul>
+                <li>Permission must be obtained in advance.
+                <li>Terraforming outside of claimed chunks is exempt from this, however try to leave a one chunk buffer between an existing build and a new build.
+            </ul>
+        <li>Claiming territory is open, however claim limits are set.
+            <ul>
+                <li>One may use chunk claiming to “fence in” an otherwise unclaimed area.
+                <li>Using claims to fence in another player to prevent them from expanding will be considered “griefing” and offending claims will be removed. Depending on the circumstances further discipline may be in order decided upon by collective staff agreement.
+                <li>Players are encouraged to settle chunk claim disputes on their own, however staff may intervene as in arbiter should a settlement be unattainable by the parties involved.
+            </ul>
+        <li>Players are also not obligated to tolerate harassment. It’s not free speech if it would promote violation of another’s personhood</li>
+            <ul>
+                <li>Some examples include but are not limited to: hate group symbols, comments in chat that would disparage against a minorities civil rights.
+                <li>Certain ideologies are not protected by this as one cannot control the circumstances of one’s birth, but one can control expressions disparaging to others.
+                <li>The current definition of hate speech employed by the server and its staff is located in definitions at the bottom of this document
+                <li>The staff will make all due efforts to remain consistent in interpretations of what constitutes hate speech, but the staff has final say and non-constructive objections to a decision may be deemed as a violation of rule 1.
+            </ul>
+        <li>If there is something that might make a good addition to the server, suggest it. 
+            <ul>
+                <li>If the idea does not get approved, do not continue badgering the staff.
+            </ul>
+        <li>Only go to bases with invitation. If not explicitly invited, ask.
+            <ul>
+                <li>Taking items without permission from another player’s base shall also be counted as a violation of this rule.
+            </ul>
+        <li>Creativity is encouraged, however keep content appropriate
+            <ul>
+                <li>Do not use the server as a platform for hate speech (see definitions), or other offensive content.
+            </ul>
+        <li>Sharing content related to builds is also encouraged, however again, do not use this as an avenue for inappropriate content. This includes sharing offensive imagery to forums/channels in which the content originated on or in some way employed the server.
+        <li>Should actual illegal activity happen on the server, the server may be obligated to comply to local and federal authorities
+            <ul>
+                <li>Legal council will be sought first in the interest of civil protections of both players and staff.
+                <li>Compliance will only be to the extent of legal obligation (i.e. warrants for logs) and no further.
+                <li>This is not a legally binding document and serves only to outline expectations and policy for behavior for all persons involved in the server both player and staff.
+            </ul>
+        <li>Violations of these rules will result in either temporary or permanent bans for offending players
+            <ul>
+                <li>Structures deemed inappropriate by staff will be destroyed
+                <li>Permanent bans must be approved by more than one staff member.
+                <li>In the case of permanent bans, an appeal may be made in written form.
+                <li>In the case of a second permanent ban, player data files may also be deleted, and their base and containing items shall be forfeit to the remaining player base.
+                <li>Should those in the server community continue to be harassed by a banned individual, staff reserves the right to report the offending person to authorities of either the academic, local, or federal jurisdiction.
+            </ul>
+    </ol>
+    
+    <p>
+    Rules subject to change without notice at staff discretion.<br>
+    Last revision: 24-Apr-2017<br><br>
+    </p>
+    <p>
+        Current Staff (by Minecraft usernames and no particular order): 
+        <ul>
+        <li>Asouer
+        <li>Linc
+        </ul>
+    </p>
+    <p>
+    Definitions:<br> 
+    Hate Speech: <a href=\"https://definitions.uslegal.com/h/hate-speech/\">uslegal.com</a> <br>
+    Griefing: 
+    Griefing is the act of irritating and angering people in video games through the use of destruction, construction, or social engineering.
+    </p>
+</div>
+";
+
+
+
+
+echo $rules;
