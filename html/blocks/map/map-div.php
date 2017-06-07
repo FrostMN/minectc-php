e0876ccc4eed008e202af98607de44ed906ef8a2
Aaron <asouer@gmail.com>
2017-06-07 14:55:48 -0500

    Create map-div.php

---
+++ html/blocks/map/map-div.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo "<div class=\"map-div\">";
+echo "    <iframe src=\"https://map.minectc.com/\" width=\"100%\" height=\"100%\">";
+echo "        <p>Your browser does not support iframes.</p>";
+echo "        <p>You can find the DynMap by clicking ";
+echo "        <a target=\"_blank\" href=\"https://map.minectc.com\">here</a>.</p>";
+echo "    </iframe>";
+echo "</div>";
