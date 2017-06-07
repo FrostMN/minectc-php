b15a00deb6ca60f33904dbe8280b18978b54264e
Aaron <asouer@gmail.com>
2017-06-07 14:46:05 -0500

    Create content-div.php

---
+++ html/blocks/home/content-div.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+switch ($current_section) {
+    case "main":
+        include ('blocks/home/main/home-div.php');
+        break;
+    case "map":
+        include ('blocks/map-div.php');
+        break;
+    case "download":
+        include ('blocks/home/download/download-div.php');
+        break;
+    case "rules":
+        include ('blocks/home/rules/rules-div.php');
+        break;
+    case "todo":
+        include ('blocks/home/todo/todo-div.php');
+        break;
+    default:
+        include ('blocks/home/main/home-div.php');
+        break;
+}
