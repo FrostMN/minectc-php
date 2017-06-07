c938b3209643060c6e4f11376a6487ebe3f39d51
Aaron <asouer@gmail.com>
2017-06-07 14:44:14 -0500

    Create all-help.php

---
+++ html/blocks/home/download/all-help.php
@@ -0,0 +1,35 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$all_help =
+"<div class =\"download-div-help\">" . 
+ 
+"
+<ol>
+    <li>Download the mod loader (forge) and the mod folder.</li>
+    <li>Open the mod loader and select install client and click ok</li>
+    <li>Open Minecraft and select the forge profile.</li>
+    <li>Load the forge profile and play (this may take some time)</li>
+    <li>Exit minecraft after it loads.</li>
+    <li>(Windows) Go into file explorer and type in %appdata%\.minecraft\mods</li>
+    <li>(MacOS) Open finder. Click the menu: Go -> Go To Folder… </li>
+
+    <li>---NEEDS Linux Specific Instruction---</li>
+    <li>---If you would like to help please contact asouer---</li>
+
+    <li>Type “~/Library/Application Support/minecraft/mods” and click Go</li>
+    <li>Drag and drop the mod folder into this folder </li>
+    <li>Extract the mods into the folder. </li>
+    <li>The next time you play on the forge profile your mods will be installed. </li>
+</ol>" .
+        
+"</div>";
+
+        
+echo $all_help;
+
